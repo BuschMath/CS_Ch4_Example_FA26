44f6fbe5ebad1c860ac7690de4845ddf9b59b020
BuschMath <busch@iowacentral.edu>
2026-09-18 10:07:19 -0500

    Updated

--- CS_Ch4_Example_FA26/Main.cpp
+++ CS_Ch4_Example_FA26/Main.cpp
@@ -7,22 +7,20 @@ int main()
 	std::ifstream infile;
 	infile.open("C:/Users/busch/source/repos/CS_Ch4_Example_FA26/CS_Ch4_Example_FA26/data.dat");
 
-	int tempInput, tempInput2;
+	int tempInput, tempInput2, tempInput3, tempInput4;
 
-	infile >> tempInput >> tempInput2;
+	infile >> tempInput >> tempInput2 >> tempInput3 >> tempInput4;
 	std::cout << "Data from the file: " << tempInput << std::endl;
 
 	std::cout << "Data from the file: " << tempInput2 << std::endl;
 
-	infile >> tempInput;
-	std::cout << "Data from the file: " << tempInput << std::endl;
+	std::cout << "Data from the file: " << tempInput3 << std::endl;
 
-	infile >> tempInput;
-	std::cout << "Data from the file: " << tempInput << std::endl;
+	std::cout << "Data from the file: " << tempInput4 << std::endl;
 
 	// File output
 	std::ofstream outHam;
-	outHam.open("output.dat");
+	outHam.open("output.dat", std::ios::app);
 
 	outHam << "Data from the file: " << tempInput << std::endl;
 	outHam << "Data from the file: " << tempInput2 << std::endl;
